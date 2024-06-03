17ef53fecff16659ea229bca8176940603c27045
abhiram945 <abhiram945@gmail.com>
2024-06-03 21:19:40 +0530

    added apk

--- src/audicApp.jsx
+++ src/audicApp.jsx
@@ -5,7 +5,7 @@ const AudicApp = () => {
         <div className="infoContainer flex flexColumn justifyCenter">
             <h3><span>AUDIC</span> is now available for Android.</h3>
             <p>Developed with React Native and Expo, the App offers the same features as website.</p>
-            <a href="https://expo.dev/artifacts/eas/tgTpLod7aJ5woK3mYpKFsh.apk" download="Audic-app">Download APK</a>
+            <a href="../assets/audic.apk" download="Audic-app">Download APK</a>
         </div>
         <div className="imagesContainer flex justifyRight alignCenter">
             <img src="../assets/App1.jpg" alt="Audic-App-1" />
